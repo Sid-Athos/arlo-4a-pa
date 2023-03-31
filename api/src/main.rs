0f0212df4794c1d98791b9248f1132be7bcf9c4c
Sid Be <sa.bennaceur@gmail.com>
2023-03-31 17:29:07 +0200

    init db

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,9 @@
+use std::env;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use sqlx::postgres::PgPoolOptions;
+extern crate dotenv;
+
+use dotenv::dotenv;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,7 +21,19 @@ async fn manual_hello() -> impl Responder {
 
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()>{
+    dotenv().ok();
+    //let conn_string = env::var("DB_URL").unwrap();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&env::var("DB_URL").unwrap()).await.expect("Database error");
+
+    // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL)
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await.expect("Failed to do the query");
+    println!("{}", row.0);
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
