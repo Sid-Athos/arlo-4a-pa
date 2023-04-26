ac2f60b378202002b312ae6f2ac02a2481ea3757
Armand <armanddailly@gmail.com>
2023-04-26 11:35:37 +0200

    retry

--- api/src/main.rs
+++ api/src/main.rs
@@ -33,7 +33,7 @@ async fn main() {
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-    println!("{} : listening on {}", "START".blue(), addr);
+    println!("{} : listening on {}", "START", addr);
 
     axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
 }
