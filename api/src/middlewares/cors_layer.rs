4e841fff9259d443511ceb357bba658b9db0da60
Sid B <sid.bennaceur@haidi.io>
2023-04-28 16:48:27 +0200

    middleware

--- api/src/middlewares/cors_layer.rs
+++ api/src/middlewares/cors_layer.rs
@@ -5,6 +5,6 @@ pub fn init_cors_layer()-> CorsLayer {
     CorsLayer::new()
         .allow_headers(Any)
         .allow_methods(Any)
-        .allow_origin(AllowOrigin::exact("http://localhost:8080".parse().unwrap()))
+        .allow_origin(AllowOrigin::exact("http://localhost:3000".parse().unwrap()))
         .expose_headers(Any)
 }
