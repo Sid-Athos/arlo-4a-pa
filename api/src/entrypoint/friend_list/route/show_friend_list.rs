e4e29049cae852494951bf52ebc3575ab1f14aa0
arnaud111 <arnaudjourdain111@gmail.com>
2023-05-27 14:30:18 +0200

    build

--- api/src/entrypoint/friend_list/route/show_friend_list.rs
+++ api/src/entrypoint/friend_list/route/show_friend_list.rs
@@ -6,6 +6,7 @@ use crate::database::repository::friend_list_repository::FriendListRepository;
 use crate::database::repository::session_repository::SessionRepository;
 use crate::database::repository::user_repository::UserRepository;
 use crate::domain::model::user::User;
+use crate::entrypoint::friend_list::route::response::friend_list_response::FriendListResponse;
 
 
 use crate::entrypoint::friend_list::route::response::user_response::UserResponse;
