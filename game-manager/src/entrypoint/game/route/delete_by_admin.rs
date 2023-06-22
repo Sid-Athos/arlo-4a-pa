032d7750a92e690e7c1198754635e36115891274
Arnaud <62839610+arnaud111@users.noreply.github.com>
2023-06-22 14:08:25 +0200

    Poule (#33)
    * Game create in bdd and in res
    
    * All game api
    
    ---------
    
    Co-authored-by: Ciapsu <59141687+Pilonim@users.noreply.github.com>

---
+++ game-manager/src/entrypoint/game/route/delete_by_admin.rs
@@ -0,0 +1,29 @@
+use axum::extract::{Path, State};
+use axum::http::StatusCode;
+use axum::{Extension, Json};
+use crate::database::init::ConnectionPool;
+use crate::domain::model::user::User;
+use crate::entrypoint::game::route::response::game_response::GameResponse;
+use crate::service::game_service::GameService;
+
+#[utoipa::path(
+get,
+path = "/game/admin/{game_id}",
+params(
+("user_id" = String,),
+),
+responses(
+(status = 200, description = "User found", body = UserResponse,),
+(status = 404, description = "User not found",),
+),
+tag = "game"
+)]
+pub async fn delete_by_admin(State(pool): State<ConnectionPool>, Path(game_id): Path<i32>) -> Result<Json<GameResponse>, StatusCode> {
+    let game_service = GameService::new(
+        pool.clone()
+    );
+
+    let game = game_service.delete_by_admin(game_id).await.unwrap();
+
+    Ok(Json(GameResponse::from_domain(game)))
+}
---
+++ game-manager/src/entrypoint/game/route/request/mod.rs
@@ -0,0 +1,2 @@
+pub mod create_game_request;
+pub mod update_game_request;
