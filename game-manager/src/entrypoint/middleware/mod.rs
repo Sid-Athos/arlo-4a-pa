5d37ffa61acc1a377ca1fca7ef17a4b3dedfef04
Arnaud <62839610+arnaud111@users.noreply.github.com>
2023-06-23 17:13:49 +0200

    Ws In Query (#37)

--- game-manager/src/entrypoint/middleware/mod.rs
+++ game-manager/src/entrypoint/middleware/mod.rs
@@ -1,2 +1,3 @@
 pub mod is_logged;
 pub mod is_logged_admin;
+pub mod is_logged_url;
--- game-manager/src/entrypoint/websocket/router.rs
+++ game-manager/src/entrypoint/websocket/router.rs
@@ -12,6 +12,7 @@ use tokio_postgres::NoTls;
 use crate::database::init::ConnectionPool;
 use crate::domain::model::user::User;
 use crate::entrypoint::middleware::is_logged::is_logged;
+use crate::entrypoint::middleware::is_logged_url::is_logged_url;
 use crate::entrypoint::websocket::connections::Connections;
 use crate::entrypoint::websocket::get_connected_friends::get_connected_friends;
 use crate::entrypoint::websocket::request::request_enum::RequestEnum;
@@ -19,9 +20,8 @@ use crate::entrypoint::websocket::request::request_enum::RequestEnum;
 pub fn ws_routes(connections: Connections, pool: Pool<PostgresConnectionManager<NoTls>>) -> Router {
 
     Router::new()
-        .route("/ws", get(ws_handler))
-        .route("/friends/connected_friends", get(get_connected_friends))
-        .layer(middleware::from_fn_with_state(pool.clone(), is_logged))
+        .route("/ws", get(ws_handler).layer(middleware::from_fn_with_state(pool.clone(), is_logged_url)))
+        .route("/friends/connected_friends", get(get_connected_friends).layer(middleware::from_fn_with_state(pool.clone(), is_logged)))
         .layer(Extension(connections.clone()))
         .with_state(pool)
 }
