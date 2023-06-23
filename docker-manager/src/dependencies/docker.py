633248c9685192ea8ff1e05b5d57936424a43584
Armand <armanddailly@gmail.com>
2023-06-23 17:30:18 +0200

    Add verification result exec

--- docker-manager/src/dependencies/docker.py
+++ docker-manager/src/dependencies/docker.py
@@ -1,4 +1,3 @@
 from docker import DockerClient
 from env import DOCKER_USERNAME, DOCKER_PASSWORD
-docker_client = DockerClient.from_env()
-docker_client.login(username=DOCKER_USERNAME, password=DOCKER_PASSWORD)
+docker_client = DockerClient.from_env().login(username=DOCKER_USERNAME, password=DOCKER_PASSWORD)
