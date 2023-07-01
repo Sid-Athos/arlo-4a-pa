c0fe017bb0c55d0e3c6825fc8ab55528284a2a9d
arnaud111 <arnaudjourdain111@gmail.com>
2023-07-01 14:34:27 +0200

    M A G N I F I Q U E x 2

--- game-manager/src/entrypoint/websocket/response/user_left_rtc_session.rs
+++ game-manager/src/entrypoint/websocket/response/user_left_rtc_session.rs
@@ -2,11 +2,11 @@ use serde::Serialize;
 
 #[derive(Serialize, Debug)]
 pub struct UserLeftRtcSessionResponse {
-    pub user_id: int,
+    pub user_id: i32,
 }
 
 impl UserLeftRtcSessionResponse {
-    pub fn new(user_id: int) -> Self {
+    pub fn new(user_id: i32) -> Self {
         Self {
             user_id
         }
