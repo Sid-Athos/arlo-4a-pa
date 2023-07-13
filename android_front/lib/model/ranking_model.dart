a1824f00efd8cc221e119aee24df6bff00ce8fe2
arnaud111 <arnaudjourdain111@gmail.com>
2023-07-13 09:12:21 +0200

    Android Ranking View

---
+++ android_front/lib/model/ranking_model.dart
@@ -0,0 +1,18 @@
+import 'package:miku/model/game_model.dart';
+import 'package:miku/model/user_model.dart';
+
+class Ranking {
+  int id;
+  int rank;
+  User user;
+  Game game;
+  int nbGames;
+
+  Ranking({
+    required this.id,
+    required this.rank,
+    required this.user,
+    required this.game,
+    required this.nbGames
+  });
+}
