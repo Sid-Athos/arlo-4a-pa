67d94491fec64d7e966390cf1d2be9b77843ed36
Dixen-Naxos <armanddailly@gmail.com>
2023-07-23 20:15:40 +0200

    Handle java correctly

--- docker-manager/dockerfiles/Dockerfile.java
+++ docker-manager/dockerfiles/Dockerfile.java
@@ -1,4 +1,6 @@
-FROM amazoncorretto:latest
+FROM eclipse-temurin:17
 ARG GAME_FILE_NAME
+COPY ./configs/pom.xml /pom.xml
 COPY ./games/"${GAME_FILE_NAME}" /game.java
-RUN javac game.java
+RUN mvn clean install
+COPY ./target/game.jar /game.jar
