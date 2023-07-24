a254b1a50eec869f586c49a97685d45482ac7d34
Ciapsu <59141687+Pilonim@users.noreply.github.com>
2023-07-24 11:25:35 +0200

    Une 415 ?? pas mal les bzez

--- game-manager/src/entrypoint/websocket/request/launch_game_request.rs
+++ game-manager/src/entrypoint/websocket/request/launch_game_request.rs
@@ -41,7 +41,7 @@ impl LaunchGameRequest {
                 },
                 Err(e) => {
                     println!("je suis la ");
-                    let val = docker_manager_service.init_rankings(lobby_member.user_id, lobby.game_id).await.map_err(status_code_to_string)?; //TODO faire marcher ça
+                    docker_manager_service.init_rankings(lobby_member.user_id, lobby.game_id).await?;
                 }
             }
         }
