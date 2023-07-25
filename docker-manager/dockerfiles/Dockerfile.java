7ed031c71d15d0be60a73e8a19aed2b971abde16
Armand <armanddailly@gmail.com>
2023-07-25 09:29:41 +0200

    Fix Java game image creation

--- docker-manager/dockerfiles/Dockerfile.java
+++ docker-manager/dockerfiles/Dockerfile.java
@@ -1,6 +1,5 @@
-FROM eclipse-temurin:17
-ARG GAME_FILE_NAME
+FROM maven:3.9.3-eclipse-temurin-17
 COPY ./configs/pom.xml /pom.xml
-COPY ./games/"${GAME_FILE_NAME}" /game.java
+COPY ./games/morpion.java /game.java
 RUN mvn clean install
 COPY ./target/game.jar /game.jar
