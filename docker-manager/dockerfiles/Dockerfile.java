831e38db90917a7030e3b5b0ac9efa49139abada
Armand <armanddailly@gmail.com>
2023-07-25 09:35:28 +0200

    Fix dockerfile java

--- docker-manager/dockerfiles/Dockerfile.java
+++ docker-manager/dockerfiles/Dockerfile.java
@@ -1,5 +1,6 @@
 FROM maven:3.9.3-eclipse-temurin-17
+ARG GAME_FILE_NAME
 COPY ./configs/pom.xml /pom.xml
-COPY ./games/morpion.java /game.java
+COPY ./games/"${GAME_FILE_NAME}" /game.java
 RUN mvn clean install
 COPY ./target/game.jar /game.jar
